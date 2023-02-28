0969b745454541e07dca361c55f7334e1b65a1c7
Alisson de Oliveira <coding@alisson.dev>
2023-02-28 22:44:18 +0000

    Fix build

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 export type {
+  isNonEmptyString,
   NonEmptyArray,
   NonEmptyString,
-  isNonEmptyString,
 } from "./non-empty";
-export type { Primitive } from "./primitive";
 export type { Option } from "./option";
 export { IsNone, IsSome, None, Some } from "./option";
-export type { SortBy, Sort } from "./sort";
-export { OrderBy } from "./sort";
+export type { Primitive } from "./primitive";
 export type { Result } from "./result";
 export { Err, IsResult, Ok, toResult } from "./result";
+export type { Sort, SortBy } from "./sort";
+export { OrderBy } from "./sort";
